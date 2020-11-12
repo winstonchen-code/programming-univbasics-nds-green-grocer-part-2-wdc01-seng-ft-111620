863fbb283b3d8f5c4dfedac3cfea5546cf5bb2f5
Winston Chen <winstonchen.wm@gmail.com>
2020-11-12 01:24:49 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,13 +4,43 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+   index = 0
+    coupons_index = 0
+
+  while coupons_index < coupons.size do
+    current_coupon = coupons[coupons_index]
+    applicable_for_discount = find_item_by_name_in_collection( current_coupon[:item], cart )
+      if ( applicable_for_discount[:count] / current_coupon[:num] >= 1 )
+        cart.push( {:item => "#{current_coupon[:item]} W/COUPON",
+                    :price => (current_coupon[:cost] / current_coupon[:num]).round(2),
+                    :clearance => applicable_for_discount[:clearance],
+                    :count => applicable_for_discount[:count] - ( applicable_for_discount[:count] % current_coupon[:num])})
+
+        applicable_for_discount[:count] %= current_coupon[:num]
+      end
+    coupons_index += 1
+  end
+  cart
 end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-end
+    index = 0
+  cart_index = 0
+  ready_for_checkout = Array.new
+
+  while cart_index < cart.size do
+    current_item = cart[cart_index]
+    if ( current_item[:clearance] )
+      current_item[:price] = current_item[:price] - ( current_item[:price] * 0.20 )
+    end
+    ready_for_checkout.push( current_item )
+    cart_index += 1 
+  end
+  ready_for_checkout
+  end
 
 def checkout(cart, coupons)
   # Consult README for inputs and outputs
@@ -22,4 +52,21 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  checkout = consolidate_cart( cart )
+  checkout = apply_coupons( checkout, coupons )
+  checkout = apply_clearance( checkout )
+
+  index = 0
+  grand_total = 0
+
+  while index < checkout.size do
+    current_item_total = checkout[index][:price] * checkout[index][:count]
+    current_item_total.round(2)
+    grand_total += current_item_total
+    index += 1
+  end
+  if ( grand_total > 100 )
+    grand_total *= 0.90
+  end
+  grand_total
 end
